c772cdee61779ff3d61ef5e221cbdbbc2de69f86
ao9792 <144122513+ao9792@users.noreply.github.com>
2025-05-12 16:26:06 +0200

    Fixade peek-funktionen och lade till tester

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log('Webpack är igång!');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+};
